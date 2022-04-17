ef19655f9362ad6f8882435db33908387f02ef9a
Jaehyun Park / 박재현 <48269869+jaehyun230@users.noreply.github.com>
2022-04-17 23:12:21 +0900

    Create solution.py

---
+++ 입국심사/solution.py
@@ -0,0 +1,18 @@
+def solution(n, times):
+    start = 0
+    end = max(times) * n
+    while start <= end :
+        mid = (start+end)//2
+        people = 0
+        for time in times :
+            people += mid // time
+            if people >= n :
+                break
+                
+        if people >= n :
+            answer = mid
+            end = mid-1
+        elif people < n :
+            start = mid + 1
+        
+    return answer
